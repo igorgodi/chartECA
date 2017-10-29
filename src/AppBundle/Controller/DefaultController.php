a5b4d76f77379ec717fc89bdda768e00c80fdf3e
Igor GODI <igor.godi@ac-reims.fr>
2017-10-29 17:23:54 +0100

    Passage en annotation template

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,19 +3,25 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
-    public function indexAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
-    }
+	/**
+	* @Route("/", name="homepage")
+	* @Template()
+	*/
+	public function indexAction(Request $request)
+	{
+
+
+		// replace this example code with whatever you need
+		return ([
+			'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+		]);
+	}
 }
