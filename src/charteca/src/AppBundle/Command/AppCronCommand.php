b4a4af8dcd98ba54a9d425f4b897e9e4e7d99f67
Igor GODI <igor.godi@ac-reims.fr>
2017-11-09 14:51:19 +0100

    Structuration du script cron

--- src/charteca/src/AppBundle/Command/AppCronCommand.php
+++ src/charteca/src/AppBundle/Command/AppCronCommand.php
@@ -3,32 +3,106 @@
 namespace AppBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Component\Console\Input\InputArgument;
+
+use Symfony\Component\Console\Command\LockableTrait;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
+/**
+ * Commande app:cron :
+ * 	Lancée par le cron toutes les heures: /chemin/bin/console app:cron --env=prod
+ */
 class AppCronCommand extends ContainerAwareCommand
 {
-    protected function configure()
-    {
-        $this
-            ->setName('app:cron')
-            ->setDescription('...')
-            ->addArgument('argument', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option', null, InputOption::VALUE_NONE, 'Option description')
-        ;
-    }
+	//--> Permet de verouiller le process : on ne peut le lancer qu'un seul à la fois
+	use LockableTrait;
 
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        $argument = $input->getArgument('argument');
+	/** Objet logger */
+	private $logger = null;
 
-        if ($input->getOption('option')) {
-            // ...
-        }
+	/** Objet de sortie écran */
+	private $output = null;
 
-        $output->writeln('Command result.');
-    }
+	/** client SOAP du webservice ECA */
+	private $wsEca = null;
+
+	/** Gestionnaire d'entitées */
+	private $em;
+
+	/**
+	 * Configuration de la commande 
+	 */
+	protected function configure()
+	{
+		$this
+		    ->setName('app:cron')
+		    ->setDescription('Tache de gestion asynchrones à lancer toutes les heures (Maintenance table utilisateurs, synchro modérateurs, gestion des quotas, désactivation des comptes)');
+	}
+
+	/**
+	 * Execution de la commande 
+	 * 
+	 * @param $input Entrée de la commande
+	 * @param $output Sortie de la commande
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		//--> On prévient les risques de lancement multiples de la commande
+		if (!$this->lock()) 
+		{
+			$output->writeln("La commande est déjà en jour d'exécution");
+			return 0;
+		}
+
+		//--> Objet d'accès à la console de sortie 
+		$this->output = $output;
+
+		//--> Objet d'accès au logger : détails https://www.remipoignon.fr/10-symfony-2-configuration-des-logs-monolog
+		$this->logger = $this->getContainer()->get('logger');
+
+		//--> Chargement du service d'accès à ECA
+		$this->wsEca = $this->getContainer()->get('app.webservice_eca');
+
+		//--> Récupération du gestionnaire d'entitées
+		$this->em = $this->getContainer()->get('doctrine')->getManager();
+		
+		//--> Création d'un id de session (pas au sens http en tout cas) qui permet de retrouver le point d'entrée dans les logs monolog
+		$this->logger->info("AppCronCommand::execute(...) : Lancement du processus croné");
+
+		//--> Tache 1 : Vérifier les utilisateurs ECA dans LDAP (AttributApplicationLocale à ECA|UTILISATEUR) et synchroniser la base des utilisateurs ChartECA
+		// TODO : $this->maintenanceUtilisateursLdap();
+
+		//--> Tache 2 : Vérifier les utilisateurs ECA dans owncloud (via le webservice dédié) et synchroniser la base des utilisateurs ChartECA
+		// TODO : $this->maintenanceUtilisateursEca();
+
+		//--> Tache 3 : Récupérer la liste des modérateurs ChartECA dans l'annuaire LDAP (AttributApplicationLocale à CHARTECA|MODERATEUR) et synchroniser la base des modérateurs ChartECA
+		$this->synchroModerateurs();
+
+		//--> Tache 4 : Vérifier les demandes d'augmentation de quota et appliquer dans ECA (via le webservice dédié)
+		// TODO : $this->traitementAugmentationQuotas();
+
+		//--> Tache 5 : Vérifier la fin des demandes d'augmentation de quota et appliquer dans ECA (via le webservice dédié)
+		// TODO : $this->traitementFinAugmentationQuotas();
+
+		//--> Tache 6 : Traiter les demandes de désactivation des comptes ECA arrivées a échénace
+		// TODO : $this->traitementDemandesDesactivationEca();
+
+	}
+
+	
+	// TODO taches 1 et 2
+
+	/**
+	 * Tache 3 : Récupérer la liste des modérateurs ChartECA dans l'annuaire LDAP et synchroniser la base des modérateurs ChartECA
+	 **/
+	private function synchroModerateurs()
+	{
+		// On journalise
+		$this->logger->info("AppCronCommand::synchroModerateurs()(...) : Exec synchronisation de la base des modérateurs ChartECA (tache 3)");
+	
+	}
+
+
+	// TODO taches 4 à 6
 
 }
