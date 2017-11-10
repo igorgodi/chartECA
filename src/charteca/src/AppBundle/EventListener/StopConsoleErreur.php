d43537ab4002612c536c1e3a3ef2c06879c8584a
Igor GODI <igor.godi@ac-reims.fr>
2017-11-10 15:19:42 +0100

    Amélioration process

--- src/charteca/src/AppBundle/EventListener/StopConsoleErreur.php
+++ src/charteca/src/AppBundle/EventListener/StopConsoleErreur.php
@@ -5,6 +5,8 @@ namespace AppBundle\EventListener;
 
 use Psr\Log\LoggerInterface;
 
+use Symfony\Component\Console\Event\ConsoleErrorEvent;
+
 class StopConsoleErreur
 {
 	// TODO comment
@@ -17,9 +19,12 @@ class StopConsoleErreur
 
 	// TODO : comment
 	// stop est chargé automatiquement lors de l'évenement console.error (voir app/config/services.yml)
-	public function stop()
+	public function stop(ConsoleErrorEvent $errorEvent, $chaine)
 	{
-		$this->logger->critical("Inerruption suite erreur en console");
+		$command = $errorEvent->getCommand();
+
+    		$this->logger->critical("ERR : " . $command->getName() . " --> $chaine");
+		$this->logger->critical("Interruption suite erreur en console");
 		exit (-1);
 
 	}
